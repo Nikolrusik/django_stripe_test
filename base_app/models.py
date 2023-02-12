051755f1b93a4307f217801cdbb101fcb0ce8391
Ruslan <nikolrusik@gmail.com>
2023-02-13 01:33:22 +0300

    Added model and migrations

--- base_app/models.py
+++ base_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(name="Name", max_length=255)
+    description = models.TextField(name="Description", null=True, blank=True)
+    price = models.FloatField(name="Price"  )
