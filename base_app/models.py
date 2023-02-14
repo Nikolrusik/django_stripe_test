e60a6652c50099287f3c9dceeca1209ab9d163f5
Ruslan <nikolrusik@gmail.com>
2023-02-14 23:18:22 +0300

    Added buy view

--- base_app/models.py
+++ base_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+import stripe
 
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(name="Name", max_length=255)
     description = models.TextField(name="Description", null=True, blank=True)
-    price = models.FloatField(name="Price"  )
+    price = models.IntegerField(name="Price", default=100)
--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, View
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Item
+import stripe
 
-# Create your views here.
+class ItemView(TemplateView):
+    template_name = "index.html"
+
+   
+
+class BuyView(View):
+     def get(self, request, pk=None):
+        item = get_object_or_404(
+            Item, pk=pk
+        )
+        product = stripe.Product.create(
+            name=item.Name,
+            description=item.Description,
+        )
+        price = stripe.Price.create(
+            unit_amount=item.Price,
+            currency="usd",
+            product=product.id,
+        )
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': price.id,
+                    'quantity': 1,
+                }
+            ],
+            mode='payment',
+            success_url = "http://localhost:8000/buy/",
+        )
+        
+        return HttpResponse(checkout_session.id)
